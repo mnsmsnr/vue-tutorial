7b42c231276457ab5fe006633795c077b60c13d5
mnsmsnr <vanillasky0127@yahoo.co.jp>
2022-06-29 18:14:06 +0900

    router追加

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,14 +1,15 @@
 <template>
   <ul id="app">
     <h1>チュートリアルToDoリスト</h1>
-     <!-- ★STEP11 -->
+    <button v-on:click="gotoVuetify()">
+      <th>Vuetifyへrouter.push（name指定）</th>
+    </button>
     <div v-for="label in options" v-bind:key="label.id">
       <input type="radio"
         v-model="current"
         v-bind:value="label.value">{{ label.label }}
     </div>
       
-    <!-- ★STEP12 -->
     （{{ computedTodos.length }} 件を表示）
     <table>
       <thead v-pre>
@@ -20,7 +21,6 @@
         </tr>
       </thead>
       <tbody>
-        <!-- ★STEP5 ToDo の要素をループ -->
         <tr
           v-for="item in computedTodos"
           v-bind:key="item.id"
@@ -28,13 +28,11 @@
           <th>{{ item.id }}</th>
           <td>{{ item.comment }}</td>
           <td class="state">
-            <!-- ★STEP10 状態変更ボタン -->
             <button v-on:click="doChangeState(item)">
               {{ labels[item.state] }}
             </button>
           </td>
           <td class="button">
-            <!-- ★STEP10 削除ボタン -->
             <button v-on:click="doRemove(item)">
               削除
             </button>
@@ -42,20 +40,15 @@
         </tr>
       </tbody>
     </table>
-    <!-- ★STEP6 -->
     <h2>新しい作業の追加</h2>
     <form class="add-form" v-on:submit.prevent="doAdd">
-      <!-- コメント入力フォーム -->
       コメント <input type="text" ref="comment">
-      <!-- 追加ボタンのモック -->
       <button type="submit">追加</button>
     </form>
   </ul>
 </template>
  
 <script>
-// ★STEP2
-// https://jp.vuejs.org/v2/examples/todomvc.html
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
   fetch: function () {
@@ -74,71 +67,48 @@ var todoStorage = {
 export default {
 
   computed: {
-
-    // ★STEP12
     computedTodos: function () {
       return this.todos.filter(function (el) {
         return this.current < 0 ? true : this.current === el.state
       }, this)
     },
-
-    // ★STEP13 作業中・完了のラベルを表示する
     labels() {
       return this.options.reduce(function (a, b) {
         return Object.assign(a, { [b.value]: b.label })
       }, {})
-      // キーから見つけやすいように、次のように加工したデータを作成
-      // {0: '作業中', 1: '完了', -1: 'すべて'}
     }
   },
-  
-  // ★STEP8
   watch: {
-    // オプションを使う場合はオブジェクト形式にする
     todos: {
-      // 引数はウォッチしているプロパティの変更後の値
       handler: function (todos) {
         todoStorage.save(todos)
       },
-      // deep オプションでネストしているデータも監視できる
       deep: true
     }
   },
-
-  // ★STEP9
   created() {
-    // インスタンス作成時に自動的に fetch() する
     this.todos = todoStorage.fetch()
   },
-  
   methods: {
-
-    // ★STEP7 ToDo 追加の処理
     doAdd: function(event, value) {
-      // ref で名前を付けておいた要素を参照
       var comment = this.$refs.comment
-      // 入力がなければ何もしないで return
       if (!comment.value.length) {
         return
       }
-      // { 新しいID, コメント, 作業状態 }
-      // というオブジェクトを現在の todos リストへ push
-      // 作業状態「state」はデフォルト「作業中=0」で作成
       this.todos.push({
         id: todoStorage.uid++,
         comment: comment.value,
         state: 0
       })
-      // フォーム要素を空にする
       comment.value = ''
     },
-
-    // ★STEP10 状態変更の処理
     doChangeState: function (item) {
       item.state = !item.state ? 1 : 0
     },
 
-    // ★STEP10 削除の処理
+    gotoVuetify: function () {
+      this.$router.push({name: 'Vuetify'})
+    },
     doRemove: function (item) {
       var index = this.todos.indexOf(item)
       this.todos.splice(index, 1)
@@ -147,11 +117,8 @@ export default {
   },
   data: function () {
     return {
-        // ★STEP5 localStorage から 取得した ToDo のリスト
         todos: [],
-        // ★STEP11 抽出しているToDoの状態
         current: -1,
-        // ★STEP11＆STEP13 各状態のラベル
         options: [
             { value: -1, label: 'すべて' },
             { value: 0, label: '作業中' },
@@ -175,7 +142,7 @@ export default {
     border-collapse: collapse;
   }
   thead th {
-    border-bottom: 2px solid #0099e4; /*#d31c4a */
+    border-bottom: 2px solid #0099e4;
     color: #0099e4;
   }
   th,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import  Todo from '@/components/Todo'
+import Todo from '@/components/Todo'
+import TodoVuetify from '@/components/TodoVuetify'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Todo',
       component: Todo
+    },
+    {
+      path: '/',
+      name: 'Vuetify',
+      component: TodoVuetify
     }
   ]
 })
